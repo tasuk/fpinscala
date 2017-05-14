1aac058bae05d4a0a24578ca8f9557df08eac3aa
Vit Brunner <vit.brunner@gmail.com>
2017-05-14 17:11:36 +0200

    Fix applicative compile errors

--- exercises/src/main/scala/fpinscala/applicative/Applicative.scala
+++ exercises/src/main/scala/fpinscala/applicative/Applicative.scala
@@ -4,7 +4,7 @@ package applicative
 import monads.Functor
 import state._
 import State._
-import StateUtil._ // defined at bottom of this file
+// import StateUtil._ // defined at bottom of this file
 import monoids._
 import language.higherKinds
 import language.implicitConversions
@@ -144,11 +144,11 @@ object Traverse {
 // The `get` and `set` functions on `State` are used above,
 // but aren't in the `exercises` subproject, so we include
 // them here
-object StateUtil {
-
-  def get[S]: State[S, S] =
-    State(s => (s, s))
-
-  def set[S](s: S): State[S, Unit] =
-    State(_ => ((), s))
-}
+// object StateUtil {
+// 
+//   def get[S]: State[S, S] =
+//     State(s => (s, s))
+// 
+//   def set[S](s: S): State[S, Unit] =
+//     State(_ => ((), s))
+// }
